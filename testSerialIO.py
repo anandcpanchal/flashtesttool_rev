6974e158d2267bf44a627814161c2fbc61d1e15d
Anand Panchal <anandp@ridlr.in>
2017-07-21 12:00:23 +0530

    testing serial inherited class #2 method change

--- testSerialIO.py
+++ testSerialIO.py
@@ -3,5 +3,5 @@ import serialIO as serial
 mySerialPort = serial.SerialPort(port='/dev/ttyS0')
 mySerialPort.clearReceiveBuffer()
 while True:
-    mySerialPort.write("AT")
+    mySerialPort.writeToPort("AT")
     mySerialPort.readFromPort(10)
